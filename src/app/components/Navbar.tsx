4ef56c5fe89bb706bab30bfed0765cfbc7652cd7
Luka Ewald <luka.ewald@gmail.com>
2024-05-22 02:53:44 +0200

    Navbar Links eingefügt

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,6 +1,7 @@
 import { Button, ButtonGroup, Flex, Heading, HStack, Spacer } from '@chakra-ui/react';
 import Login from '../login/page';
 import { MdLogin } from "react-icons/md"
+import Link from 'next/link';
 
 export default function Navbar() {
 
@@ -11,28 +12,47 @@ export default function Navbar() {
       }
   }
   return (
-    <Flex as='nav' padding='10px' alignItems='center' gap='10px' bg='teal.700'>
+    <Flex as='nav' padding='10px' alignItems='center' gap='10px' bg='teal.700' wrap='wrap'>
         <Heading as='h1' color='white'>Gruppe 5</Heading>
-        
         <Spacer />
-        <HStack spacing='36px'>
-            <ButtonGroup variant='link' spacing='36px'>
-                <Button color='white'>Startseite</Button>
-                <Button color='white'>Alle Bücher</Button>
-                <Button color='white'>Erweiterte Suche</Button>
-                <Button color='white'>Neuanlegen</Button>
-            </ButtonGroup>
-            <Button
-                variant='outline'
-                color='white'
-                colorScheme='teal.500'
-                rightIcon={<MdLogin />}
-                sx={loginButtonStyles}
-            >
-                    Login
-            </Button>
-        </HStack>
 
+        <HStack spacing='36px' wrap='wrap'>
+            <ButtonGroup variant='link' spacing='36px'>
+                <Link
+                    href='/'
+                >
+                    <Button color='white'>Startseite</Button>
+                </Link>
+                <Link
+                    href='suchen'
+                >
+                    <Button color='white'>Alle Bücher</Button>
+                </Link>
+                <Link
+                    href='suchen'
+                >
+                    <Button color='white'>Erweiterte Suche</Button>
+                </Link>
+                <Link
+                    href='erstellen'
+                >
+                    <Button color='white'>Neuanlegen</Button>
+                </Link>
+            </ButtonGroup>
+            <Link
+                    href='login'
+                >
+                <Button
+                    variant='outline'
+                    color='white'
+                    colorScheme='teal.500'
+                    rightIcon={<MdLogin />}
+                    sx={loginButtonStyles}
+                >
+                        Login
+                </Button>
+            </Link>
+        </HStack>
     </Flex>
   )
 }
