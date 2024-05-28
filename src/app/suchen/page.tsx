24d59001930bd02460e8d8fbf1ab1d8900d667d7
Luka Ewald <luka.ewald@gmail.com>
2024-05-28 22:34:52 +0200

    Anzeige der Suchergebnisse (Layout)

--- src/app/suchen/page.tsx
+++ src/app/suchen/page.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import { useEffect, useState } from 'react';
-import { Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, HStack, SimpleGrid, Spacer, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { Badge, Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, HStack, Icon, SimpleGrid, Spacer, Stack, Text, useColorModeValue } from "@chakra-ui/react";
 import AdvancedSearch from "../components/AdvancedSearch";
 import getBuecher from "../lib/getBuecher";
-import { FaEye } from "react-icons/fa6";
+import { FaEye, FaStar } from "react-icons/fa6";
 
 export default function Suchen({
   searchParams
@@ -23,71 +23,88 @@ export default function Suchen({
     fetchBuecher();
   }, [searchParams])
 
-  // isbn: string;
-  // rating: number;
-  // art: string;
-  // preis: number;
-  // lieferbar: boolean;
-  // schlagwoerter: string[];
-  // titel: {
-  //   titel: string;
-  //   untertitel: string;
-  // };
+  const StarRating = ({ rating }) => {
+    const stars = Array(5).fill(0);
+  
+    return (
+      <HStack>
+        {stars.map((_, index) => (
+          <Icon
+            as={FaStar}
+            key={index}
+            color={index < rating ? 'teal.500' : 'gray.300'}
+            boxSize={5}
+            style={{ marginTop: '8px' }}
+          />
+        ))}
+      </HStack>
+    );
+  };
+
+  const Lieferbar = ({ lieferbar }) => {
+    return (
+      <Badge
+        colorScheme={lieferbar ? 'green' : 'red'}
+        variant="subtle"
+        fontSize="sm"
+        fontWeight="500"
+        px={3}
+        rounded="full"
+        m={1}
+      >
+        {lieferbar ? 'LIEFERBAR' : 'NICHT LIEFERBAR'}
+      </Badge>
+    );
+  };
 
   return (
-    <SimpleGrid>
+    <Box p={4}>
       <AdvancedSearch />
-      <SimpleGrid spacing={10} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
+      <SimpleGrid spacing={10} p={4} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
         {buecher && buecher.map(buch => (
-          <Card key={buch.isbn} borderTop='8px' borderColor='teal.400'>
+          <Card key={buch.isbn} borderTop='8px' borderColor='teal.400' bg='white'>
             <CardHeader>
-              <Flex justifyContent='space-between' alignItems='center'>
-                <Stack>
-                  <HStack>
-                    <Box display='flex' justifyContent='space-between' alignItems='center'>
-                      <Text
+              <Flex direction='column' w='100%'>
+                <Flex justify='space-between' alignItems='center' w='100%'>
+                    <Text
                       color={'teal.500'}
                       textTransform={'uppercase'}
                       fontWeight={800}
                       fontSize={'sm'}
                       letterSpacing={1.1}>
-                        {buch.art}
-                      </Text>
-                      <Button variant='ghost' colorScheme='teal' leftIcon={<FaEye />}>Details</Button>
-                    </Box>
-                  </HStack>
-                  <Heading size='md'>{buch.titel.titel}</Heading>
-                  {/* TODO Rating */}
-                </Stack>
+                      {buch.art}
+                    </Text>
+                    <Button variant='ghost' colorScheme='teal' leftIcon={<FaEye />}>Details</Button>
+                </Flex>
+                <Heading size='md'>{buch.titel.titel}</Heading>
+                <StarRating rating={buch.rating} />
               </Flex>
             </CardHeader>
             <CardBody>
-              <Text>{buch.schlagwoerter && buch.schlagwoerter.map((schlagwort, index) => (
-                <Text key={index}
-                  fontSize={'sm'}
-                  fontWeight={500}
-                  bg={'teal.50'}
-                  p={2}
+              {buch.schlagwoerter && buch.schlagwoerter.map((schlagwort, index) => (
+                <Badge
+                  key={index}
+                  fontSize='sm'
+                  fontWeight='500'
+                  colorScheme='teal'
                   px={3}
-                  color={'green.500'}
-                  rounded={'full'}
+                  rounded='full'
+                  m={1}
                 >
                   {schlagwort}
-                </Text>
+                </Badge>
               ))}
-              </Text>
             </CardBody>
             <Divider borderColor={'gray.200'} />
             <CardFooter>
-              <HStack>
-                {/* TODO Preisdarstellung */}
-                <Text color='teal.600' fontSize='xl'>{buch.preis}€</Text>
-                {/* TODO <Text>lieferbar?</Text> */}
+              <HStack justify='space-between' alignItems='center' w='100%'>
+                <Text color='teal.600' fontSize='xl'>{buch.preis.toFixed(2).replace('.', ',')}€</Text>
+                <Lieferbar lieferbar={buch.lieferbar} />
               </HStack>
             </CardFooter>
           </Card>
         ))}
       </SimpleGrid>
-    </SimpleGrid>
+    </Box>
   );
 }
