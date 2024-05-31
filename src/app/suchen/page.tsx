58edd3c0df1c7368baf44480521fa4b0ca58facb
Nico Rieß <riess.nico@web.de>
2024-05-31 15:39:03 +0200

    'Alle Bücher' & 'Erweiterte Suche' zeigt jetzt alle Bücher an und 'Keine Bücher gefunden' falls keine vorhanden

--- src/app/suchen/page.tsx
+++ src/app/suchen/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect, useState } from 'react';
-import { Badge, Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, HStack, Icon, SimpleGrid, Spacer, Stack, Text, useColorModeValue } from "@chakra-ui/react";
+import { Badge, Box, Button, Card, CardBody, CardFooter, CardHeader, Divider, Flex, Heading, HStack, Icon, SimpleGrid, Text } from "@chakra-ui/react";
 import AdvancedSearch from "../components/AdvancedSearch";
 import getBuecher from "../lib/getBuecher";
 import { FaEye, FaStar } from "react-icons/fa6";
@@ -17,15 +17,25 @@ export default function Suchen({
 
   useEffect(() => {
     const fetchBuecher = async () => {
-      const buecherData: Promise<Buch[]> = getBuecher(`titel=${searchParams.titel}`);
-      setBuecher(await buecherData);
+      let query = '';
+      if (searchParams.titel) {
+        query = `titel=${searchParams.titel}`;
+      }
+      try {
+        const buecherData: Buch[] = await getBuecher(query);
+        console.log('Empfangene Bücher:', buecherData); // Loggen Sie die empfangenen Daten
+        setBuecher(buecherData);
+      } catch (error) {
+        console.error('Fehler beim Abrufen der Bücher:', error);
+        setBuecher([]);
+      }
     }
     fetchBuecher();
-  }, [searchParams])
+  }, [searchParams]);
 
   const StarRating = ({ rating }) => {
     const stars = Array(5).fill(0);
-  
+
     return (
       <HStack>
         {stars.map((_, index) => (
@@ -61,11 +71,12 @@ export default function Suchen({
     <Box p={4}>
       <AdvancedSearch />
       <SimpleGrid spacing={10} p={4} templateColumns='repeat(auto-fill, minmax(250px, 1fr))'>
-        {buecher && buecher.map(buch => (
-          <Card key={buch.isbn} borderTop='8px' borderColor='teal.400' bg='white'>
-            <CardHeader>
-              <Flex direction='column' w='100%'>
-                <Flex justify='space-between' alignItems='center' w='100%'>
+        {buecher.length > 0 ? (
+          buecher.map(buch => (
+            <Card key={buch.isbn} borderTop='8px' borderColor='teal.400' bg='white'>
+              <CardHeader>
+                <Flex direction='column' w='100%'>
+                  <Flex justify='space-between' alignItems='center' w='100%'>
                     <Text
                       color={'teal.500'}
                       textTransform={'uppercase'}
@@ -75,35 +86,38 @@ export default function Suchen({
                       {buch.art}
                     </Text>
                     <Button variant='ghost' colorScheme='teal' leftIcon={<FaEye />}>Details</Button>
+                  </Flex>
+                  <Heading size='md'>{buch.titel.titel}</Heading>
+                  <StarRating rating={buch.rating} />
                 </Flex>
-                <Heading size='md'>{buch.titel.titel}</Heading>
-                <StarRating rating={buch.rating} />
-              </Flex>
-            </CardHeader>
-            <CardBody>
-              {buch.schlagwoerter && buch.schlagwoerter.map((schlagwort, index) => (
-                <Badge
-                  key={index}
-                  fontSize='sm'
-                  fontWeight='500'
-                  colorScheme='teal'
-                  px={3}
-                  rounded='full'
-                  m={1}
-                >
-                  {schlagwort}
-                </Badge>
-              ))}
-            </CardBody>
-            <Divider borderColor={'gray.200'} />
-            <CardFooter>
-              <HStack justify='space-between' alignItems='center' w='100%'>
-                <Text color='teal.600' fontSize='xl'>{buch.preis.toFixed(2).replace('.', ',')}€</Text>
-                <Lieferbar lieferbar={buch.lieferbar} />
-              </HStack>
-            </CardFooter>
-          </Card>
-        ))}
+              </CardHeader>
+              <CardBody>
+                {buch.schlagwoerter && buch.schlagwoerter.map((schlagwort, index) => (
+                  <Badge
+                    key={index}
+                    fontSize='sm'
+                    fontWeight='500'
+                    colorScheme='teal'
+                    px={3}
+                    rounded='full'
+                    m={1}
+                  >
+                    {schlagwort}
+                  </Badge>
+                ))}
+              </CardBody>
+              <Divider borderColor={'gray.200'} />
+              <CardFooter>
+                <HStack justify='space-between' alignItems='center' w='100%'>
+                  <Text color='teal.600' fontSize='xl'>{buch.preis.toFixed(2).replace('.', ',')}€</Text>
+                  <Lieferbar lieferbar={buch.lieferbar} />
+                </HStack>
+              </CardFooter>
+            </Card>
+          ))
+        ) : (
+          <Text>Keine Bücher gefunden</Text>
+        )}
       </SimpleGrid>
     </Box>
   );
