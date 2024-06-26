eb3ca61bae7741d106ef1c3868f21e0be2bf9dcc
Nico Rieß <riess.nico@web.de>
2024-06-26 12:19:58 +0200

    style: NewBook changed to CreateBook

--- src/app/create/page.tsx
+++ src/app/create/page.tsx
@@ -1,14 +1,14 @@
 'use client';
 import { Box, Flex, useToast } from '@chakra-ui/react';
 import HorizontalBar from '../components/common/HorizontalBar';
-import NewBook from '../components/books/NewBook';
+import CreateBook from '../components/books/CreateBook';
 
 export default function Create() {
   return (
     <Box>
       <HorizontalBar title="Anlegen" subtitle="Starte dein neues Kapitel" />
       <Flex direction="column" align="center" p={4}>
-        <NewBook />
+        <CreateBook />
       </Flex>
     </Box>
   );
